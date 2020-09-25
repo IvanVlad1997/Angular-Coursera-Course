10e28a7e292c7a7e777dc08cc673c2da53438c2b
IvanVlad1997 <ivanvlad97@yahoo.ro>
2020-09-25 12:46:14 +0300

    SPA part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Dish } from '../shared/dish'
+import { DishService } from '../services/dish.service'
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  dish: Dish;
+  promotion: Promotion;
+
+  constructor( private dishService: DishService,
+    private promotionservice: PromotionService) { }
+
+  ngOnInit()  {
+    this.dish = this.dishService.getFeaturedDish();
+    this.promotion = this.promotionservice.getFeaturedPromotion();
   }
 
 }
