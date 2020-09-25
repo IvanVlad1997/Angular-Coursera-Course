77db5c3a82ba6896032a78f8a2d731a4b7b79438
IvanVlad1997 <ivanvlad97@yahoo.ro>
2020-09-25 14:51:12 +0300

    template-driven forms part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { MatToolbarModule} from '@angular/material/toolbar'
-import {FlexLayoutModule} from '@angular/flex-layout'
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule} from '@angular/material/grid-list'
 import { MatCardModule} from '@angular/material/card'
 import { MatButtonModule} from '@angular/material/button'
 import { MatDialogModule } from '@angular/material/dialog'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox'
+
+import {FlexLayoutModule} from '@angular/flex-layout'
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
@@ -19,6 +24,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component'
+ 
 
 
 import { DishService} from './services/dish.service';
@@ -53,6 +59,12 @@ import { LoginComponent } from './login/login.component'
     MatButtonModule,
     AppRoutingModule,
     MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
+
   ],
   providers: [
     DishService,
